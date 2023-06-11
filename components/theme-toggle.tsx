ec1030dde9f33a0a5296148e00d6f485b217fbe1
kobejk <105041728+kobejk@users.noreply.github.com>
2023-06-11 15:15:59 +1000

    fix: hydration error when loading theme

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -3,21 +3,32 @@
 import * as React from "react";
 import { useTheme } from "next-themes";
 
+import { useState, useEffect } from "react";
+
 import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 
 export function ThemeToggle() {
-    const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, theme } = useTheme();
 
-    return (
-        <Button
-            variant="ghost"
-            size="sm"
-            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        >
-            <Sun className="rotate-0 scale=100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle Theme</span>
-        </Button>
-    );
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      size="sm"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      <Sun className="rotate-0 scale=100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle Theme</span>
+    </Button>
+  );
 }
